f0a1102b4e21f92ea5bbb087a5742b6eeecbd95a
KuangjuX <18630816527@163.com>
2021-09-07 10:10:10 +0800

    update

--- tistore/db.py
+++ tistore/db.py
@@ -1,51 +1,19 @@
-import pymysql
+import influxdb
 
-class RetionalDB:
-    def __init__(self, config: dict):
-        self.user = config['user']
-        self.pwd = config['pwd']
-        self.host = config['host']
-        self.db = config['db']
-    
+class DB:
+    def __init__(self):
+        self.host = ""
+        self.port = 0
+        self.username = ""
+        self.password = ""
+
     def connect(self):
-        cnx = pymysql.connect(
-            user = self.user,
-            password = self.pwd,
-            host = self.host,
-            database = self.db
+        client = influxdb.InfluxDBClient(
+            host=self.host,
+            port=self.port, 
+            username=self.username, 
+            password=self.password,
+            ssl=True,
+            verify_ssl=True
         )
-        self.connect = cnx
-        self.cursor = self.connect.cursor()
-
-    def close(self):
-        self.connect.close()
-
-    async def store(self, table: str, meta: dict):
-        '''
-        Store metadate to Table
-        '''
-
-        sql = "INSERT INTO %s (`dataset`, `field`, `objid`, `object` \
-            `ra`, `dec`, `mag`, `created`) \
-            VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
-        self.cursor.execute(
-            sql, 
-            (
-                table,
-                meta['DATASET'], meta['FIELD'], 
-                meta['OBJID'], meta['OBJECT'], 
-                meta['RA'], meta['DEC'], 
-                meta['MAG'], meta['CREATED']
-            )
-        )
-
-
-class NoRetionalDB:
-    def __init__(self, config: dict):
-        self.user = config['user'] 
-        self.pwd = config['pwd']
-        self.host = config['host']
-        self.db = config['db']
-
-    async def store(self, table: str, data: dict):
-        print("No implement")
+        self.client = client
