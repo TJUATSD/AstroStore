3eddb9ff15ca6d0d79ad97050f8f402b1d944816
Annie1900 <73414003+Annie1900@users.noreply.github.com>
2021-10-06 19:34:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from astrostore.manager import Manager
-# from tistore.utils import parse_config
+from astrostore.parse.csv import ParseCSV
+from astrostore.db.influxdb import InfluxDB
 import asyncio
 
 def main():
     filename = "./testdata/62000006.csv"
-    manager = Manager()
-    manager.parse_csv(filename)
+    parse1 = ParseCSV(filename)
+    file = parse1.open()
+    data = parse1.read(file)
+    parse1.parse(data)
+    t1 = parse1.timedata()
+    influxdb = InfluxDB()
+    influxdb.write_csv_data("test", t1)
 
 if __name__ == '__main__':
     main()
